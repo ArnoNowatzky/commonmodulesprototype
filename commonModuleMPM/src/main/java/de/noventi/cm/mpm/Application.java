01eb8f129a94332d6d5391547b4717593ca30e8e
oleyma <markus.oley@vsa.de>
2020-12-22 10:51:35 +0100

    Jenkinsfile

---
+++ commonModuleMPM/src/main/java/de/noventi/cm/mpm/Application.java
@@ -0,0 +1,9 @@
+package de.noventi.cm.mpm;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class Application {
+}
