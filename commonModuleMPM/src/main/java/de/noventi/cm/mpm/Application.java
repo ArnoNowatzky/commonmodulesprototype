5f73f2d3a40cd8dc5cc186e28a905cdf6ad3d970
oleyma <markus.oley@vsa.de>
2020-12-23 23:56:57 +0100

    build ui

--- commonModuleMPM/src/main/java/de/noventi/cm/mpm/Application.java
+++
@@ -1,9 +0,0 @@
-package de.noventi.cm.mpm;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication
-@ComponentScan
-public class Application {
-}
