784204efe2e153fe0ed286f241e0ec17d5623c5d
oleyma <markus.oley@vsa.de>
2021-01-21 12:42:32 +0100

    event

--- commonModuleRuntime/src/main/java/de/noventi/cm/runtime/RuntimeApplication.java
+++ commonModuleRuntime/src/main/java/de/noventi/cm/runtime/RuntimeApplication.java
@@ -1,17 +1,24 @@
 package de.noventi.cm.runtime;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.activemq.broker.BrokerService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan
 @EnableScheduling
+@Slf4j
 public class RuntimeApplication {
 
   public static void main(final String[] args) {
     SpringApplication.run(RuntimeApplication.class, args);
   }
+
+
 
 }
--- commonModuleRuntime/src/main/java/de/noventi/cm/runtime/event/config/ActiveMQConfig.java
+++ commonModuleRuntime/src/main/java/de/noventi/cm/runtime/event/config/ActiveMQConfig.java
@@ -3,6 +3,7 @@ package de.noventi.cm.runtime.event.config;
 import javax.jms.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,15 @@ public class ActiveMQConfig {
 
   @Value("${active-mq.broker-url}")
   private String brokerUrl;
+
+/**  @Bean embedded broker
+  public BrokerService broker() throws Exception {
+    //TODO for docker runtime the broker must run in docker (docker-compose)
+    log.info("Configure broker with broker url " + brokerUrl);
+    BrokerService broker = new BrokerService();
+    broker.addConnector(brokerUrl);
+    return broker;
+  }**/
 
   @Bean
   public ConnectionFactory connectionFactory (){
